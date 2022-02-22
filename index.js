12c8686f017d95c3144d00940e8bae230456afdc
SebLeyr <sebastien.leyrissoux@gmail.com>
2022-02-22 10:54:53 +0100

    correction

--- index.js
+++ index.js
@@ -3,38 +3,33 @@ Vue.createApp({
         return { 
             leNombre : 0,
             result : 0,
-            afficherResult : "",
-            time : 0
         };
     },
+    computed: {
+        afficherResult() {
+            if(this.leNombre === 7) {
+                return this.leNombre;
+            } else if(this.leNombre < 7) {
+                return "Essaie encore !";
+            } else {
+                return "Tu as dépassé le nombre mystère";
+            }
+        }
+    },
     methods : {
-        reset(){
-            this.leNombre = 0;
-            this.afficherResult = "Temps écoulé !";
-        },
-        timeOut(){
-            this.time = setTimeout(this.reset(), 5000);
-        },
         leNombrePlus1(){
             this.leNombre = this.leNombre + 1;
-            clearTimeout(this.time);
-            this.timeOut();
         },
         leNombrePlus5(){
             this.leNombre = this.leNombre + 5;
-            clearTimeout(this.time);
-            this.timeOut();
         },
     },
     watch: {
         leNombre(value) {
-            if(value == 7) {
-                this.result = this.leNombre;
-            } else if(value < 7) {
-                this.afficherResult = "Essaie encore !";
-            } else if(value > 7) {
-                this.afficherResult = "Tu as dépassé le nombre mystère"
-            }
-        }
-    }
+            console.log('le watcher en action...');
+            setTimeout(() => {
+            this.leNombre = 0;
+            }, 5000);
+        },
+    },
 }).mount('#monApp');
